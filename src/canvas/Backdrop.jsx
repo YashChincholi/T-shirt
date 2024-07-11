8f967f48da4c239d04ff5613a3da48cf004ea0a9
Your Name <you@example.com>
2024-07-11 20:13:16 +0530

    styled the tabs

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -9,7 +9,7 @@ const Backdrop = () => {
       ref={shadows}
       temporal
       frames={60}
-      alphaTest={0.4}
+      alphaTest={0.3}
       scale={10}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
@@ -17,14 +17,14 @@ const Backdrop = () => {
       <RandomizedLight
         amount={4}
         radius={9}
-        intensity={0.55}
+        intensity={0.45}
         ambient={0.25}
         position={[5, 5, -10]}
       />
       <RandomizedLight
         amount={4}
         radius={5}
-        intensity={0.25}
+        intensity={0.45}
         ambient={0.55}
         position={[-5, 5, -9]}
       />
